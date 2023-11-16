d0a66465cd6b1113c046e0ccc630c2b0ee4b6b5a
sushmakamuju <131590836+sushmakamuju@users.noreply.github.com>
2023-11-17 00:01:12 +0530

    Add files via upload

---
+++ Authentication/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authentication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        AuthenticationViewModel Authenticator; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            Authenticator = new();
+        }
+        private async void AuthenticateButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> res = await Authenticator.AuthenticateUser();
+            if (res[0] == "true")
+            {
+
+                MessageBox.Show("Authenticated", "Authentication Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            else
+            {
+                MessageBox.Show("Authenticated", "Authentication not Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+
+
+        }
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+     
+        
+   
+      
+       
+    }
+}
